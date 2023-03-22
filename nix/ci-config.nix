94b6c1249695e0d3139356108dde2d047774880a
Emily Martins <emi@haskell.fyi>
2023-03-22 17:31:26 +0000

    add comment on hercules config

--- nix/ci-config.nix
+++ nix/ci-config.nix
@@ -67,7 +67,11 @@ in
       );
   };
   config = {
+    # NOTE(Emily, 22 Mar 2023): Here we provide sensible defaults for working with liqwid-nix. 
+    # These can be overwritten by the user, though. In the future, we may want some out-of-the-
+    # -box implementation for enabling this somehow.
     flake = { ... }: {
+      # Added in: 2.7.2.
       config.herculesCI = {
         ciSystems = [ "x86_64-linux" ];
         onPush.default.outputs = self.checks.x86_64-linux;
